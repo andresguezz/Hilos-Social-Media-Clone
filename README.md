32fd02f53200b580bacde29253ca69fb14553db1
andresguezz <jarodriguez3203@misena.edu.co>
2024-01-31 23:20:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## HILOS
+Social media clone of threads made to show my fullstack advances 
+
+
+the autentification was made posible thanks to Clerk
+https://clerk.com/
---
+++ middleware.ts
@@ -0,0 +1,16 @@
+// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
+// Copy the middleware code as it is from the above resource
+
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  
+  publicRoutes: ["/api/webhook/clerk"],
+
+  
+  ignoredRoutes: ["/api/webhook/clerk"],
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
